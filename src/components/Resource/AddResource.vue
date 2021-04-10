beb86f73bd5a00112184be683a66ed9c5d4bcd7a
arup-paul <aruppaulctg@gmail.com>
2021-04-10 12:31:24 -0700

    fetching user input

--- src/components/Resource/AddResource.vue
+++ src/components/Resource/AddResource.vue
@@ -1,17 +1,17 @@
 <template>
     <base-card>
-      <form>
+      <form @submit.prevent="submitData()">
           <div class="form-control"> 
             <label for="title"> Title</label>
-            <input type="text " id="title" name="title">
+            <input type="text " id="title" name="title" ref="titleInput" />
           </div>
           <div class="form-control"> 
             <label for="description"> Description</label>
-           <textarea name="description" id="description"  rows="3"></textarea>
+           <textarea name="description" id="description" ref="descInput"  rows="3"></textarea>
           </div>
           <div class="form-control"> 
             <label for="link"> Link</label>
-            <input type="text " id="link" name="link">
+            <input type="text " id="link" name="link" ref="linkInput" />
           </div>
           <div>
               <base-button :type="submit">Add Resource</base-button>
@@ -20,6 +20,20 @@
     </base-card>
 </template>
 
+<script>
+export default {
+    inject: ['addResource'],
+    methods: {
+        submitData() {
+           const enteredTitle = this.$refs.titleInput.value;
+           const enteredDescription = this.$refs.descInput.value;
+           const enteredUrl = this.$refs.linkInput.value;
+
+          this.addResource(enteredTitle,enteredDescription,enteredUrl);
+        }
+    }
+    }
+</script>
 
 <style scoped>
   label {
